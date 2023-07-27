e9dd5b8479e709a576bd442cca162d4e92dac7a6
Deepak-Sangode <dpsangode29@gmail.com>
2023-07-27 16:21:55 +0530

    [Deepak] Committing my 1st Code

---
+++ src/test/java/eCommerceProject/Flipkart.java
@@ -0,0 +1,17 @@
+package eCommerceProject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Flipkart {
+  @Test
+  public void flipkartTest() throws InterruptedException {
+	  WebDriver driver = new ChromeDriver();
+	  Reporter.log("Opening Flipkart",true);
+	  driver.get("https://www.flipkart.com/");
+	  Thread.sleep(5000);
+	  
+  }
+}
